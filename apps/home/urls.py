b47cea440b5a44cac367583c43e15035315353ab
Alan Viars <aviars@videntity.com>
2019-08-28 13:29:32 -0400

    Update front page test/demonstration logic.

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
 from django.contrib import admin
-from .views import authenticated_home, id_token_payload_json
+from .views import authenticated_home, id_token_payload_json, fetch_cda
 
 admin.autodiscover()
 
 urlpatterns = [
+    
+    url(r'fecth-cda-from-hie', fetch_cda, name='fetch_cda'),
+    url(r'', authenticated_home, name='home'),
     url(r'id-token-payload', id_token_payload_json, name='id_token_payload_json'),
+
+    
+    
     url(r'', authenticated_home, name='home'),
 ]
