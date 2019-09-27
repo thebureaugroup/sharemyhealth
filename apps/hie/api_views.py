8cba7fa875e34390e2b94cbd965047ebf779c424
Sean Harrison <sah@bookgenesis.com>
2019-09-27 16:29:39 -0500

    Only fetch the patient FHIR data from HIXNY if ALL of the following are true: * the request.user is the resource owner, AND * the data is empty or more than 24-hrs old, AND * user explicitly requests refresh.

--- apps/hie/api_views.py
+++ apps/hie/api_views.py
@@ -1,8 +1,10 @@
+from datetime import timedelta
 import json
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse, FileResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_GET
+from django.utils import timezone
 from oauth2_provider.decorators import protected_resource
 from collections import OrderedDict
 from .models import HIEProfile
@@ -13,13 +15,31 @@ from . import hixny_requests
 @require_GET
 @protected_resource()
 def get_patient_fhir_content(request):
-    user = request.resource_owner
-    up, g_o_c = UserProfile.objects.get_or_create(user=user)
-    hp, g_o_c = HIEProfile.objects.get_or_create(user=user)
-    hie_data = hixny_requests.fetch_patient_data(user, hp, up)
-    if not hie_data.get('error'):
-        hp.__dict__.update(**hie_data)
-        hp.save()
+    """Only fetch the patient FHIR data from HIXNY if ALL of the following are true:
+
+    * the request.user is the resource owner, AND
+    * the data is empty or more than 24-hrs old, AND 
+    * user explicitly requests refresh.
+    """
+    owner = request.resource_owner
+    hp, g_o_c = HIEProfile.objects.get_or_create(user=owner)
+
+    if (
+        request.user == owner
+        and (
+            not hp.fhir_content
+            or (timezone.now() - hp.updated_at) > timedelta(hours=24)
+        )
+        and request.GET.get('refresh', None)
+    ):
+        up, g_o_c = UserProfile.objects.get_or_create(user=owner)
+        hie_data = hixny_requests.fetch_patient_data(owner, hp, up)
+        if not hie_data.get('error'):
+            hp.__dict__.update(**hie_data)
+            hp.save()
+    elif not hp.fhir_content:
+        hie_data = {'error': "FHIR content is not available"}
+
     if hp.fhir_content:
         return JsonResponse(json.loads(hp.fhir_content))
     else:
